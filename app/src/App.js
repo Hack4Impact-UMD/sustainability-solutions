2c57c31d4ab05d65eb2d2399abae44fef1f248ab
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-10-17 16:57:29 -0400

    Organized folders and added navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,19 @@
-import AboutBoardMembers from './Components/AboutBoardMembers/aboutBoardMembers';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import React from 'react';
-
-import Landing from './Pages/Landing';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AboutBoardMembers from './Components/AboutBoardMembers/aboutBoardMembers';
+import LandingPage from './Components/LandingPage/Landing';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
       <div className="app">
+        <Navbar/>
         <Switch>
+          <Route exact path="/" component={LandingPage}/>
           <Route exact path="/aboutBoardMembers" component={AboutBoardMembers}/>
         </Switch>
       </div>
