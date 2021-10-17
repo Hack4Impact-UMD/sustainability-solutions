a4b0f0becd984b9a419166bca31918577c73a28a
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-10-17 14:44:59 -0400

    Added text and image to about page

--- app/src/Components/AboutBoardMembers/aboutBoardMembers.js
+++ app/src/Components/AboutBoardMembers/aboutBoardMembers.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import './aboutBoardMembers';
+import React from "react";
+import "./aboutBoardMembers";
+import styles from "./aboutBoardMembers.css";
 
 function aboutBoardMembers() {
-    return (
-        <div className="board-members">
-            This is about the board members.
-        </div>
-    )
+  return (
+    <div className="board-members">
+      <div className={styles.boardMember}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src="professional.jpeg"></img>
+      </div>
+    </div>
+  );
 }
 
-export default aboutBoardMembers
+export default aboutBoardMembers;
