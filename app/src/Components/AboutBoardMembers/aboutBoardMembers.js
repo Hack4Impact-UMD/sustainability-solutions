ff43a95e98f30044dbe944571046e254681929c8
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-10-17 12:46:52 -0400

    Created preliminary about board members page

---
+++ app/src/Components/AboutBoardMembers/aboutBoardMembers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './aboutBoardMembers';
+
+function aboutBoardMembers() {
+    return (
+        <div className="board-members">
+            This is about the board members.
+        </div>
+    )
+}
+
+export default aboutBoardMembers
