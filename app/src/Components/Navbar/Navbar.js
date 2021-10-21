4eafe675cdae80fe0ae2382598263778c934370d
Ethan Huegler <ethan@huegler.net>
2021-10-20 20:27:49 -0400

    created inital navabr

--- app/src/Components/Navbar/Navbar.js
+++ app/src/Components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css'
+import logo from './draft-logo.png'
 
 function Navbar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/aboutBoardMembers">About Board Members</Link>
-        </div>
+        <header className="navbar">
+            <div className="brand">
+                <img src={logo} alt="logo"></img>
+                <div className="title">SSI</div>
+            </div>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/">Our Team</Link>
+                <Link to="/">Contact Us</Link>
+                <Link to="/">News</Link>
+                <Link to="/aboutBoardMembers">About Board Members</Link>
+            </nav>
+        </header>
     )
 }
 
