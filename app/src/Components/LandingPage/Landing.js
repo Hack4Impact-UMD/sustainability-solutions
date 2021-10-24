544c3620cd9dcb9aa95ffa111f6c90689bce428e
Julie <jyang00@terpmail.umd.edu>
2021-10-24 14:31:43 -0400

    initial creation of background for all sections

--- app/src/Components/LandingPage/Landing.js
+++ app/src/Components/LandingPage/Landing.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+
+import styles from './landing.css';
+
+import Background1 from './background1.png';
+import Background2 from './background2.png';
 
 const Landing = (props) => {
 
     return (
         <div>
-            <p>Hello, This is the landing Page</p>
+            <div>
+                <Image className={styles.background1} src={Background1} alt={'Intro image'} />
+                <p className={styles.title}>
+                    Sustainability <br />
+                    Solutions  <br />
+                    Institute
+                </p>
+            </div>
+            <div className={styles.section2}>
+
+            </div>
+            <div>
+                <Image className={styles.background2} src={Background2} alt={'Intro image'} />
+            </div>
         </div>
     );
 }
