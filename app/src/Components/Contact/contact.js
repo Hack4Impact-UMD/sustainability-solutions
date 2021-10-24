73e93f341abd777aa46b663800cafe6155f9fcb6
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-10-24 11:25:41 -0400

    Created contact form with react-bootstrap

--- app/src/Components/Contact/contact.js
+++ app/src/Components/Contact/contact.js
@@ -1,7 +1,45 @@
 import styles from "./contact.css";
+import { Form, Row, Col, FloatingLabel, Button } from "react-bootstrap";
 
 function contact() {
+  return (
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridFirstName">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="fname" placeholder="John" />
+        </Form.Group>
 
+        <Form.Group as={Col} controlId="formGridLastName">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lname" placeholder="Smith" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group controlId="formGridEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group>
+        <FloatingLabel controlId="messagearea" label="Message">
+          <Form.Control
+            as="textarea"
+            placeholder="Leave your message here"
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+      </Form.Group>
+
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check this box" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default contact;
