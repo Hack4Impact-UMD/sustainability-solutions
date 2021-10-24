81f0d21190b8bdf6b8097af6cf4e6c9fc63aeabf
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-10-24 14:54:59 -0400

    Trying to get form to display, installed react-bootstrap and react-router-dom again

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,23 @@
-import AboutBoardMembers from './Components/AboutBoardMembers/aboutBoardMembers';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import AboutBoardMembers from "./Components/AboutBoardMembers/aboutBoardMembers";
+import Contact from "./Components/Contact/contact.js";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'/>
-      <div className="app">
-        <Switch>
-          <Route exact path="/aboutBoardMembers" component={AboutBoardMembers}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat"
+          rel="stylesheet"
+        />
+        <div className="app">
+          <Switch>
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
