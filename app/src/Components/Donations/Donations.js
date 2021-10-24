02b75a7dbbb75abed57ed93a7b58470b0dd810e9
Julie <jyang00@terpmail.umd.edu>
2021-10-24 18:00:52 -0400

    added empty pages for each link in nav and updated link paths in App.js

---
+++ app/src/Components/Donations/Donations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Donations = (props) => {
+
+    return (
+        <div>
+            <p>Hello, This is the donations Page</p>
+        </div>
+    );
+}
+
+export default Donations;
