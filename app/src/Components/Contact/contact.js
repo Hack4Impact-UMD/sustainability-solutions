5abafc1eb49ef5004ae1f8c288ad413058f53a62
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-10-27 20:55:34 -0400

    Added into image to contact page

--- app/src/Components/Contact/contact.js
+++ app/src/Components/Contact/contact.js
@@ -1,44 +1,54 @@
 import styles from "./contact.css";
-import { Form, Row, Col, FloatingLabel, Button } from "react-bootstrap";
+import { Image, Form, Row, Col, FloatingLabel, Button } from "react-bootstrap";
+import { render } from "react-dom";
+import IntroImage from "./IntroImage.png";
 
 function contact() {
   return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="fname" placeholder="John" />
+    <div>
+      <Image
+        className={styles.image}
+        src={IntroImage}
+        alt={"Contact us image"}
+      />
+
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="fname" placeholder="John" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="lname" placeholder="Smith" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="lname" placeholder="Smith" />
+        <Form.Group>
+          <FloatingLabel controlId="messagearea" label="Message">
+            <Form.Control
+              as="textarea"
+              placeholder="Leave your message here"
+              style={{ height: "100px" }}
+            />
+          </FloatingLabel>
         </Form.Group>
-      </Row>
 
-      <Form.Group controlId="formGridEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
+        <Form.Group className="mb-3" id="formGridCheckbox">
+          <Form.Check type="checkbox" label="Check this box" />
+        </Form.Group>
 
-      <Form.Group>
-        <FloatingLabel controlId="messagearea" label="Message">
-          <Form.Control
-            as="textarea"
-            placeholder="Leave your message here"
-            style={{ height: "100px" }}
-          />
-        </FloatingLabel>
-      </Form.Group>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check this box" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 }
 
