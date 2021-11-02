9f97c6df23a407f28f74943e5b31815f4ce3b04f
Julie <jyang00@terpmail.umd.edu>
2021-11-01 20:26:07 -0400

    added intro and news sections

--- app/src/Components/LandingPage/Landing.js
+++ app/src/Components/LandingPage/Landing.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
-import styles from './landing.css';
+import './landing.css';
 
-import Background1 from './background1.png';
-import Background2 from './background2.png';
+import NewsImage from './NewsImage.png';
 
-const Landing = (props) => {
-
+function Landing () {
     return (
-        <div>
+        <div className="landing-container">
             <div>
-                <Image className={styles.background1} src={Background1} alt={'Intro image'} />
-                <p className={styles.title}>
-                    Sustainability <br />
-                    Solutions  <br />
-                    Institute
+                <p className="intro">
+                    SUSTAINABILITY <br />
+                    SOLUTIONS <br />
+                    INSTITUTE
                 </p>
+                <p className="intro-cap">embrace pathways of sustainability</p>
             </div>
-            <div className={styles.section2}>
-
+            <div className="section2">
+                <p>Hello</p>
             </div>
-            <div>
-                <Image className={styles.background2} src={Background2} alt={'Intro image'} />
+            <div className="news-sec">
+                <h1>Current News</h1>
+                <div className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner news-slides">
+                        <div className="carousel-item">
+                            <img
+                                className="news-image"
+                                src={NewsImage}
+                            />
+                            <p className="news-info"> 
+                                Title <br/>
+                                <br/>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore magna
+                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
