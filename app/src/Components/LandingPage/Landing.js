ee0a01c5c1a157cba7697b2bbd924ca27eabe9db
Julie <jyang00@terpmail.umd.edu>
2021-11-06 21:47:24 -0400

    added a non working carousel

--- app/src/Components/LandingPage/Landing.js
+++ app/src/Components/LandingPage/Landing.js
@@ -32,7 +32,45 @@ function Landing () {
                 </Link>
             </div>
             <div className="news-sec">
-                <h1>Current News</h1>
+                <h1>News Highlight</h1>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=373940"
+                        alt="First slide"
+                        />
+                        <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+  {/*
                 <div className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner news-slides">
                         <div className="carousel-item">
@@ -51,6 +89,7 @@ function Landing () {
                         </div>
                     </div>
                 </div>
+*/}
             </div>
         </div>
     );
