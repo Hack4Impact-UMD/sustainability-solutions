4faba907ca313a07a695d710a6a57184bcc5ef82
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2021-11-07 10:56:34 -0700

    Added container so form looks somewhat nicer

--- app/src/Components/Contact/contact.js
+++ app/src/Components/Contact/contact.js
@@ -1,5 +1,13 @@
 import styles from "./contact.module.css";
-import { Image, Form, Row, Col, FloatingLabel, Button } from "react-bootstrap";
+import {
+  Container,
+  Image,
+  Form,
+  Row,
+  Col,
+  FloatingLabel,
+  Button,
+} from "react-bootstrap";
 import { render } from "react-dom";
 import IntroImage from "./IntroImage.png";
 
@@ -13,43 +21,51 @@ function contact() {
           alt={"Contact us image"}
         />
       </div>
-      <div>
-        <Form>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridFirstName">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="fname" placeholder="John" />
-            </Form.Group>
+      <div className={styles.container}>
+        <Container fluid>
+          <Row>
+            <Col>
+              <div className={styles.form}>
+                <Form>
+                  <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridFirstName">
+                      <Form.Label>First Name</Form.Label>
+                      <Form.Control type="fname" placeholder="John" />
+                    </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridLastName">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="lname" placeholder="Smith" />
-            </Form.Group>
+                    <Form.Group as={Col} controlId="formGridLastName">
+                      <Form.Label>Last Name</Form.Label>
+                      <Form.Control type="lname" placeholder="Smith" />
+                    </Form.Group>
+                  </Row>
+
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                  </Form.Group>
+
+                  <Form.Group as={Col} className="message">
+                    <FloatingLabel controlId="messagearea" label="Message">
+                      <Form.Control
+                        as="textarea"
+                        placeholder="Leave your message here"
+                        style={{ height: "100px" }}
+                      />
+                    </FloatingLabel>
+                  </Form.Group>
+
+                  <Form.Group as={Col} className="mb-3" id="formGridCheckbox">
+                    <Form.Check type="checkbox" label="Check this box" />
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </div>
+            </Col>
           </Row>
-
-          <Form.Group controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group>
-            <FloatingLabel controlId="messagearea" label="Message">
-              <Form.Control
-                as="textarea"
-                placeholder="Leave your message here"
-                style={{ height: "100px" }}
-              />
-            </FloatingLabel>
-          </Form.Group>
-
-          <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="Check this box" />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+        </Container>
       </div>
     </div>
   );
