5b35cd059e5eb156710e7253e0d8e0b0f2fd9bd1
Aryan Das <adas1@umd.edu>
2021-11-13 09:49:37 -0500

    u

--- app/src/Components/Contact/contact.js
+++ app/src/Components/Contact/contact.js
@@ -20,43 +20,62 @@ function contact() {
           alt={"Contact us image"}
         />
       </div>
+      <div className={styles.welcometext}>
+        <p>
+          Have questions? Send us a message!
+        </p>
+      </div>
       <div className={styles.container}>
         <Container fluid>
           <Row>
             <Col>
               <div className={styles.form}>
                 <Form>
-                  <Row className="mb-3">
+                  <Row className="formName">
                     <Form.Group as={Col} controlId="formGridFirstName">
-                      <Form.Label>First Name</Form.Label>
+                      <Form.Label>First Name*</Form.Label>
                       <Form.Control type="fname" placeholder="John" />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridLastName">
-                      <Form.Label>Last Name</Form.Label>
+                      <Form.Label>Last Name*</Form.Label>
                       <Form.Control type="lname" placeholder="Smith" />
                     </Form.Group>
                   </Row>
 
-                  <Row className="mb-3">
+                  <Row className="formEmail">
                     <Form.Group as={Col} controlId="formGridEmail">
+                      <Form.Label className="heading">Email*</Form.Label>
                       <Form.Control type="email" placeholder="Enter email" />
                     </Form.Group>
                   </Row>
 
-                  <Row className="mb-3">
-                    <Form.Group as={Col} className="message">
-                      <FloatingLabel controlId="messagearea" label="Message">
-                        <Form.Control
-                          as="textarea"
-                          placeholder="Leave your message here"
-                          style={{ height: "100px" }}
-                        />
-                      </FloatingLabel>
+                  <Row className="formPhoneNumber">
+                    <Form.Group as={Col} controlId="formGridNumber">
+                      <Form.Label className="heading">Phone Number</Form.Label>
+                      <Form.Control type="phonenumber" placeholder="(###)-(###)-(####)" />
                     </Form.Group>
                   </Row>
 
-                  <Button variant="primary" type="submit">
+                  <Row className="formSubject">
+                    <Form.Group as={Col} controlId="formGridSubject">
+                      <Form.Label className="heading">Subject*</Form.Label>
+                      <Form.Control type="subject" placeholder="Enter subject" />
+                    </Form.Group>
+                  </Row>
+
+                  <Row className="formMessage">
+                    <Form.Group as={Col} className="message">
+                      <Form.Label className="heading">Message*</Form.Label>
+                      <Form.Control
+                        as="textarea"
+                        placeholder="Leave your message here"
+                        style={{ height: "100px" }}
+                      />
+                    </Form.Group>
+                  </Row>
+
+                  <Button className="button" variant="primary" type="submit">
                     Submit
                   </Button>
                 </Form>
