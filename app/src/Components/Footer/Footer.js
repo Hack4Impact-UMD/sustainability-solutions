e3bfe544c74d21ee2308899c2167dc9161bac573
Julie Yang <57774664+jyang00@users.noreply.github.com>
2021-11-13 23:23:41 -0500

    Initial Completed Footer (#18)
    * initial footer
    
    * added css styling and white bars into footer
    
    * changed name of org

---
+++ app/src/Components/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import InstagramIcon from './instagram-brands.svg';
+import FacebookIcon from './facebook-square-brands.svg';
+import TwitterIcon from './twitter-brands.svg';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="logo-col">
+                <p>
+                    Sutainability <br/>
+                    Solutions <br/>
+                    Group
+                </p>
+                <Link to="/contactus">
+                    <button> CONTACT US </button>
+                </Link>
+            </div>
+            <div className="white-bar" />
+            <div className="links-col">
+                <Link to="/"><p>Home</p></Link>
+                <Link to="/about"><p>Team</p></Link>
+                <Link to="/mission"><p>Mission</p></Link>
+                <Link to="/news"><p>News</p></Link>
+                <Link to="/donations"><p>Donations</p></Link>
+            </div>
+            <div className="white-bar" />
+            <div className="icon-col">
+                <a herf="#"><img className="icons" src={InstagramIcon}></img></a> <br/>
+                <a herf="#"><img className="icons" src={FacebookIcon}></img></a> <br/>
+                <a herf="#"><img className="icons" src={TwitterIcon}></img></a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
