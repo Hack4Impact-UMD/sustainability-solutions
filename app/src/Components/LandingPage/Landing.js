3126a81c129be8b22a5a8704d09f60e9501b7703
Julie <jyang00@terpmail.umd.edu>
2021-11-14 01:10:00 -0500

    updated image css and added a section for image captions in the news section

--- app/src/Components/LandingPage/Landing.js
+++ app/src/Components/LandingPage/Landing.js
@@ -32,63 +32,47 @@ function Landing () {
             </div>
             <div className="news-sec">
                 <h1>News Highlight</h1>
-                <Carousel>
+                <Carousel className="news-slides">
                     <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
-                        alt="First slide"
-                        />
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
-                        alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
-                        alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-  {/*
-                <div className="carousel slide" data-ride="carousel">
-                    <div className="carousel-inner news-slides">
-                        <div className="carousel-item">
-                            <img
-                                className="news-image"
-                                src={NewsImage}
-                            />
-                            <p className="news-info"> 
-                                Title <br/>
-                                <br/>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                                ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        <img src={NewsImage} />
+                        <div className="news-caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit  
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                                irure dolor in reprehenderit in voluptate velit esse cillum 
+                                dolore eu fugiat nulla pariatur. 
                             </p>
                         </div>
-                    </div>
-                </div>
-*/}   
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={NewsImage} />
+                        <div className="news-caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit  
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                                irure dolor in reprehenderit in voluptate velit esse cillum 
+                                dolore eu fugiat nulla pariatur. 
+                            </p>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={NewsImage} />
+                        <div className="news-caption">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit  
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                                irure dolor in reprehenderit in voluptate velit esse cillum 
+                                dolore eu fugiat nulla pariatur. 
+                            </p>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     );
