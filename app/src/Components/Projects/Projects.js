0eea5d788a0114f7a15fc7800f2cc2befc62413d
Sadena Rishindran <sadena.rishindran@gmail.com>
2021-11-20 18:29:22 -0500

    Improved carousel styling

--- app/src/Components/Projects/Projects.js
+++ app/src/Components/Projects/Projects.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import "./projects.css";
 import NewsImage from "./meet_the_board_header.jpg"
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
 
 const Projects = () => {
+    const prev = <GrFormPrevious size={70}/>
+    const next = <GrFormNext size={70}/>
+
     return (
         <div>
             <div className="project-header-border">
@@ -15,46 +19,124 @@ const Projects = () => {
             </div>
             <div className="projects">
                 <h2>Current Projects</h2>
-                <Carousel>
+                <Carousel prevLabel={null} nextLabel={null} prevIcon={prev} nextIcon={next}>
                     <Carousel.Item>
                         <div className="item">
-                            <figure>
-                                <img src={NewsImage} className="proj"/>
-                                <figcaption className="name">Description</figcaption>
-                                <figcaption className="position">Month</figcaption>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
                             </figure>
-                            <figure>
-                                <img src={NewsImage} className="proj"/>
-                                <figcaption className="name">Description</figcaption>
-                                <figcaption className="position">Month</figcaption>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
                             </figure>
-                            <figure>
-                                <img src={NewsImage} className="proj"/>
-                                <figcaption className="name">Description</figcaption>
-                                <figcaption className="position">Month</figcaption>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
                             </figure>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={NewsImage} className="proj"/>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit  
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                laboris nisi ut aliquip ex ea commodo consequat.
-                            </p>
+                        <div className="item">
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
                         </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img src={NewsImage} className="proj"/>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit  
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                                laboris nisi ut aliquip ex ea commodo consequat.
-                            </p>
+                        <div className="item">
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div className="projects">
+                <h2>Past Projects</h2>
+                <Carousel prevLabel={null} nextLabel={null} prevIcon={prev} nextIcon={next}>
+                    <Carousel.Item>
+                        <div className="item">
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 1</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="item">
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 2</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="item">
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
+                            <figure className="project">
+                                <img src={NewsImage} className="project-img"/>
+                                <figcaption className="project-name">Description 3</figcaption>
+                                <figcaption className="month">Month 00, 0000</figcaption>
+                            </figure>
                         </div>
                     </Carousel.Item>
                 </Carousel>
