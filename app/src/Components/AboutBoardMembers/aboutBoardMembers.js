a3a16d7413ea56304354dd4999f7c790043cab79
Sadena Rishindran <sadena.rishindran@gmail.com>
2022-01-18 20:14:25 -0500

    Added Camellia board member

--- app/src/Components/AboutBoardMembers/aboutBoardMembers.js
+++ app/src/Components/AboutBoardMembers/aboutBoardMembers.js
@@ -8,15 +8,17 @@ import Jill from "./Images/Jill Gambill.jpg"
 import Karen from "./Images/Karen Campblin.jpg"
 import Sarah from "./Images/Sarah Williams.jpg"
 import William from "./Images/William Harris.jpg"
+import Camellia from "./Images/Camellia Moses Okpodu.jpg"
 import headerImage from './Images/our team.png';
 
 const team = [
-  {img: Karen, name: "Karen Campblin", position: "Chair", description: ""},
+  {img: Karen, name: "Karen Campblin", position: "Chair", description: "Virginia State Conference Climate and Energy Justice Leader Initiative. Founded in 1935, the Virginia State Conference of NAACP Branches (Virginia NAACP) is the oldest and largest nonpartisan civil rights organization in the Commonwealth, overseeing over 100 NAACP branches, youth councils, and college chapters. She is also the Co Founder and Leader of the Virginia Green New Deal a state wide coalition of environmental, social justice, and advocacy groups who focus on the design of policy to advance clean energy, transportation, social justice, equitable workforce among others. The Green New Deal Coalition proposes to create thousands of good jobs while addressing the climate emergency and restoring the Virginia environment."},
   {img: Eric, name: "Eric Harris", position: "Secretary", description: "Senior Project Manager and Program Specificialist Cisco Systems"},
-  {img: Sarah, name: "Sarah Williams", position: "Board Member", description: ""},
+  {img: Sarah, name: "Dr. Sarah Williams", position: "Board Member", description: "Licensed Clinical Psychologist/Executive Coach, also has her own television show, Ask Dr. Sarah, and works with at-risk youth. Developed a love of the water and the environment as a native of the Florida panhandle. Serves on the Chesapeake Redevelopment and Housing Authority. Doctor of Philosophy, Education; Education Specialist Degree, MS, Clinical Mental Health Counseling."},
   {img: Eilleen, name: "Eilleen Woll", position: "Board Member", description: "Sierra Club, Director of Off Shore Wind"},
   {img: Jill, name: "Jill Gambill", position: "Board Member", description: "Public Service faculty at the University of Georgia, serving as the Coastal Community Resilience Specialist for UGA Marine Extension and Georgia Sea Grant"},
   {img: William, name: "William Harris", position: "Board Member", description: "Project Manager, Computer Systems Specialist"},
+  {img: Camellia, name: "Camellia Moses Okpodu", position: "Board Member", description: "Professor and Dean School of Arts and Sciences University of Wyoming, Former Dean of Arts and Science at Xavier University New Orleans"},
 ]
 
 function AboutBoardMembers() {
